04dfd6f5762a01555ff49980b15a6c8d5eeca174
Tommy Reijs <tommy.reijs@gmail.com>
2023-08-22 15:47:02 +0200

    added slash commands maybe

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const client = new Client({
 
 client.commands = new Collection();
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in teh collection with hte key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log('[WARNING] The command at ${filePath} is missing a required "data" or "execute" property');
+    }
+}
+
 client.on('ready', () => {
     console.log('Ready, set, go!')
 })
@@ -25,5 +39,25 @@ client.on('messageCreate', message => {
         message.reply('E')
     }
 })
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error('No command matching ${interaction.commandName} was found.');
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+});
 
 client.login(process.env.TOKEN)
