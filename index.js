9ddad52d24c91ab1b53712a4137aa69362008d0a
Tommy Reijs <tommy.reijs@gmail.com>
2023-08-22 14:27:19 +0200

    added intentions

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
     ],
 })
 
