155b375a1e6a6d91aed7ad5b98a5d6336254f891
Tommy Reijs <tommy.reijs@gmail.com>
2023-08-22 15:10:37 +0200

    Added individual files for commands

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('test')
+            .setDescription('test 123'),
+        async execute(interaction) {
+            // just replies with a basic test test test to make sure slash commands are working
+            await interaction.reply('test test test')
+        },
+    };
